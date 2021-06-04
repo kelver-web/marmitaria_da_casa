cb63e189bfdc07adeff7195814203b55eb12e642
kelver-web <65473037+kelver-web@users.noreply.github.com>
2021-06-04 17:41:33 -0300

    Update settings.py

--- marmitaria/settings.py
+++ marmitaria/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME', 'marmitaria'),
         'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASS', 'jiujitsu1'),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
         'HOST': 'localhost',
         'PORT': '5432',
     }
