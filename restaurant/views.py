be1927df232fe7af92c31635dae8a425ff89e6ab
kelver <kelverwt@gmail.com>
2021-06-04 18:03:44 -0300

    criação de teste da primeira página

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('<h1>Aqui será a página principal da marmitaria da casa</h1>')
