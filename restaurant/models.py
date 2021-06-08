f0ec350ebc8dd1a51a6fbbdb6349fdcb919add92
kelver <kelverwt@gmail.com>
2021-06-08 14:11:18 -0300

    mudança na classe pedido

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+
 class Pedido(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     proteinas = models.CharField(max_length=100)
