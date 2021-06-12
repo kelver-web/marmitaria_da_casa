1cb2cb26001633ce94c4e7330b03c5f223a1598f
kelver <kelverwt@gmail.com>
2021-06-12 18:13:12 -0300

    criado a lógica de somar o pedido

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,12 +10,12 @@ class Pedido(models.Model):
     proteinas = models.CharField(max_length=100)
     acompanhamentos = models.CharField(max_length=100)
     saladas = models.CharField(max_length=80)
-    valor_refeicao = models.DecimalField(max_digits=10, decimal_places=2)
-    quantidade_refeicao = models.CharField(max_length=20)
+    valor_refeicao = models.FloatField()
+    quantidade_refeicao = models.IntegerField()
     sabor_bebida250 = models.CharField(max_length=20, verbose_name='sabor de 250ml')
-    quantidade_bebida_250 = models.CharField(max_length=20, verbose_name='quantidade bebida 250ml')
+    quantidade_bebida_250 = models.IntegerField(verbose_name='quantidade bebida 250ml')
     bebida_1lt = models.CharField(max_length=15)
-    quantidade_bebida_1lt = models.CharField(max_length=20)
+    quantidade_bebida_1lt = models.IntegerField()
     nome = models.CharField(max_length=50)
     email = models.EmailField(blank=True)
     telefone = models.CharField(max_length=20)
@@ -28,7 +28,12 @@ class Pedido(models.Model):
     def __str__(self):
         return self.nome
 
+    def soma(self):
+        total = (self.quantidade_refeicao * self.valor_refeicao) + (self.quantidade_bebida_250 * 2.50 + self.quantidade_bebida_1lt * 5.00)
+        return (f'{total:.2f}')
 
+
+    
 class Cadapio(models.Model):
     nome = models.CharField(max_length=40)
     proteina = models.CharField(max_length=70) 
